5183bf7cd3ebd32ac1ac9e820b99c1dd35d2d4be
AyubFoks <ayubfoks@live.com>
2025-07-15 11:23:58 +0300

    Innitial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <header>
+      Events Manager
+     </header>
+     <p>
+      Welcome to our event manager.
+     </p>
+    {/*Component	Purpose
+App	Root component; holds main state (events array)
+EventList	Renders all events; receives events as props
+EventItem	Displays a single event + Edit/Delete buttons
+EventForm	Form to create or edit events
+EventFilter	(Optional) Filters or searches the events
+Home	Page combining EventList, EventForm, and Filter*/}
     </div>
   );
 }
